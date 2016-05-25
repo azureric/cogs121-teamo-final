9538f2f70247f93d2768506985e4b2e2bd460bf7
azureric <ericfriday@outlook.com>
2016-05-24 19:27:12 -0700

    Fix the throw err in d3

--- routes/queryDELPH.js
+++ routes/queryDELPH.js
@@ -1,69 +1,65 @@
 var pg = require('pg');
 var conString = process.env.DATABASE_CONNECTION_URL;
 
-exports.map_anxiety_rate = function (req, res) {
+exports.map_anxiety_rate = function(req, res) {
     var client = new pg.Client(conString);
     client.connect(function(err) {
-        if(err) {
+        if (err) {
             return console.error('could not connect to postgres', err);
 
         }
         client.query('SELECT "Geography", "2010 Hospitalization No.", "2011 Hospitalization No.", "2012 Hospitalization No." FROM cogs121_16_raw.hhsa_anxiety_disorder_2010_2012 AS tableData',
             function(err, result) {
-                if(err) {
+                if (err) {
                     return console.error('error running query', err);
                 }
 
                 var rawData = result.rows;
                 console.log("current raw data is: " + rawData);
 
-
                 var returnGeoData = [];
                 var maxAnxSumNumber = 0;
                 var thisAreaSum = 0;
                 var vaildGeoCounter = 0;
 
-                for(i = 0; i < rawData.length; i++){
+                for (i = 0; i < rawData.length; i++) {
                     var renderGeoData = {};
 
                     var checkVaildGeoName = String(rawData[i]["Geography"]);
 
-                    if((checkVaildGeoName.indexOf("Region") > -1) ||
+                    if ((checkVaildGeoName.indexOf("Region") > -1) ||
                         (checkVaildGeoName.indexOf("Unknown") > -1) ||
-                        (checkVaildGeoName.indexOf("Rate") > -1)){
+                        (checkVaildGeoName.indexOf("Rate") > -1)) {
                         continue;
                     }
 
                     renderGeoData["area"] = checkVaildGeoName;
 
                     var number2010 = rawData[i]["2010 Hospitalization No."];
-                    if(number2010 == "<5"){
+                    if (number2010 == "<5") {
                         number2010 = 5;
-                    }
-                    else{
+                    } else {
                         number2010 = parseInt(number2010);
                     }
 
                     var number2011 = rawData[i]["2011 Hospitalization No."];
-                    if(number2011 == "<5"){
+                    if (number2011 == "<5") {
                         number2011 = 5;
-                    }
-                    else{
+                    } else {
                         number2011 = parseInt(number2010);
                     }
 
                     var number2012 = rawData[i]["2012 Hospitalization No."];
-                    if(number2012 == "<5"){
+                    if (number2012 == "<5") {
                         number2012 = 5;
-                    }
-                    else{
+                    } else {
                         number2012 = parseInt(number2010);
                     }
 
                     thisAreaSum = number2010 + number2011 + number2012;
                     renderGeoData["yearSum"] = thisAreaSum;
 
-                    if(maxAnxSumNumber < thisAreaSum){
+                    if (maxAnxSumNumber < thisAreaSum) {
                         maxAnxSumNumber = thisAreaSum;
                     }
                     returnGeoData.push(renderGeoData);
@@ -71,7 +67,7 @@ exports.map_anxiety_rate = function (req, res) {
                     vaildGeoCounter++;
                 }
 
-                for(i = 0; i < vaildGeoCounter; i++){
+                for (i = 0; i < vaildGeoCounter; i++) {
                     returnGeoData[i]["ratio"] = returnGeoData[i]["yearSum"] / maxAnxSumNumber;
                 }
 
@@ -81,5 +77,7 @@ exports.map_anxiety_rate = function (req, res) {
             });
     });
 
-    return { anxietyData: "No data present." }
+    return {
+        anxietyData: "No data present."
+    }
 }
